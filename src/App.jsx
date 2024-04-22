53df6d0e9d733cf518f62da8e77e8451fe26c73a
Terry Cheng <cyr062599@gmail.com>
2024-04-22 14:25:13 +0930

    Update ESLint configuration and add fetchCities function in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000/cities";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
