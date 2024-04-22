03c426dbfae7f9e4935e61941844d575073e8261
Terry Cheng <cyr062599@gmail.com>
2024-04-22 22:50:47 +0930

    Implement delete city function

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -13,6 +13,10 @@ function CityItem({ city }) {
   const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
+  function handleClick(e) {
+    e.preventDefault();
+  }
+
   return (
     <li>
       <Link
@@ -24,7 +28,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
